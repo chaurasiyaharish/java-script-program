775b2762e7bb27283b6e919b5077cdd4d0ed8b07
chaurasiyaharish <chaurasiyah348@gmail.com>
2024-09-06 19:36:50 +0530

    added file

---
+++ crude04-agust/update.js
@@ -0,0 +1,13 @@
+async function dataShow(){
+    let mydata =`
+    <table>
+    <tr>
+    <>th>Employee no</th>
+     <>th>city</th>
+      <>th>Salary</th>
+       <>th>Action</th>
+    `;
+    let url = "http//"
+    let mydata = await myobj.json();
+    
+} 
